7e9cabc8daae843d19d07ccac732c53020ebaa16
ueabu <umaabu@yahoo.co.uk>
2022-01-30 01:17:07 -0800

    added refresh and more

---
+++ Refresh.py
@@ -0,0 +1,20 @@
+from Secrets import refresh_token, base_64
+import requests
+
+class Refresh:
+    def __init__(self):
+        self.refresh_token = refresh_token
+        self.base_64 = base_64
+
+    def refresh(self):
+        query = "https://accounts.spotify.com/api/token"
+        response = requests.post(query,
+                                 data={"grant_type": "refresh_token",
+                                       "refresh_token": refresh_token},
+                                 headers={"Authorization": "Basic " + base_64})
+        response_json = response.json()
+        print(response_json)
+        return response_json["access_token"]
+
+# a = Refresh()
+# a.refresh()
